c26b563248d39ba389b3fb8735cba0c35f0d0591
Angel <angelvalangelov@gmail.com>
2023-05-13 23:20:28 +0300

    Sliders

---
+++ Tkinter_course/sliders.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+root.title('Radio buttons')
+root.geometry('400x400')
+
+vertical = Scale(root, from_=0, to=200)
+vertical.pack()
+
+horizontal = Scale(root, from_=0, to=200, orient=HORIZONTAL)
+horizontal.pack()
+
+mainloop()

d31fa7028b591269c94fba738befbca039285574
Angel <angelvalangelov@gmail.com>
2023-05-14 22:06:59 +0300

    Checkboxes

---
+++ Tkinter_course/checkboxes.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+root = Tk()
+root.title('Radio buttons')
+root.geometry('400x400')
+
+var = StringVar()
+
+
+def show():
+    Label(root, text=var.get()).pack()
+
+
+c = Checkbutton(root, text='Are you agree?', variable=var, onvalue='Yes', offvalue='No')
+c.deselect()
+c.pack()
+
+myButton = Button(root, text='Show selection', command=show).pack()
+
+mainloop()
